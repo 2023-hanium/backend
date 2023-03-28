a8e0fe02a76b003a502949e4659c6077c74ede83
Ray Lee <wooo519@naver.com>
2023-03-29 00:32:08 +0900

    chore : hide django setting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,11 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4a@y%-q2b&5!e+nvesx442vw!ve46t(vxrfd8t8#2u3gnoldda'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
